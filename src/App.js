bcb90bba21d648e92ce911305d76286aab2e7c8f
Zafuzi <zach@sleepless.com>
2019-07-23 15:23:31 -0700

    First steps towards using categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Logo from './logo.png';
-import { BrowserRouter as Router, Route  } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Home from './components/Home.js';
 import Article from './components/Article.js';
 
@@ -9,6 +9,7 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			category: "",
 			articles: [],
 			refreshArticles: false
 		}
@@ -20,15 +21,26 @@ class App extends React.Component {
 		}
 	}
 
+	componentDidUpdate = () => {
+		if(this.state.articles.length == 0)
+			this.getArticles();
+	}
+
 	getArticles = () => {
-		fetch('https://wt-bc10bd70eac1ba2e1f09bbd50514e6a3-0.sandbox.auth0-extend.com/newlyread')
+		let url = 'https://wt-bc10bd70eac1ba2e1f09bbd50514e6a3-0.sandbox.auth0-extend.com/newlyread'; 
+		if(this.state.category != "") {
+			url += `?category=${this.state.category}`
+		}
+		fetch(url)
 		.then(res => res.json())
 		.then(json => {
-			console.log("Getting Articles");
 			this.setState({articles: json.articles});
 		});
 	}
 
+	setCategory = (c) => {
+		this.setState({articles: [], category: c || ""});
+	}
 
 	render() {
 		return (
@@ -36,6 +48,12 @@ class App extends React.Component {
 			  <div className="App">
 				<header>
 					<img className="App-logo" src={Logo} alt="loading logo"/> <h1>ewlyread</h1>
+					<nav>
+						<a href="/">All</a>
+						<Link to={ {pathname: "/", search: `?category=technology` } }>
+							Technology
+						</Link>
+					</nav>
 				</header>
 				<Route exact path="/" component={(props) => <Home articles={this.state.articles}/>} />
 				<Route path="/article" component={Article} />
